731fc2917c5903e1f0a43828da6e92216d1283ce
Anthony TASTET <atastet@e2r1p2.42.fr>
2018-05-24 15:34:07 +0200

    [v0.2] First version Uncheck

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,32 @@
 /*   By: atastet <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/18 12:21:22 by atastet           #+#    #+#             */
-/*   Updated: 2018/05/24 14:01:29 by atastet          ###   ########.fr       */
+/*   Updated: 2018/05/24 15:33:37 by atastet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+static char		*write_line(t_lst *tmp)
+{
+	char	buf[BUF_SIZE + 1];
+	int		i;
+	char	*tmp_rd;
+	int		ret;
+	
+	i = 0;
+	while ((ret = (read(tmp->fd, buf, BUF_SIZE)) > 0) &&
+		ft_strchr(tmp->txt, '\n') == NULL)
+	{
+		buf[ret] = '\0';
+		tmp_rd = tmp->txt;
+		tmp->txt = ft_strjoin(tmp_rd, buf);
+		free(tmp_rd);
+	}
+	if (ret < 0)
+		return (NULL);
+	return (tmp->txt);
+}
 
 static t_lst	*fd_new(int fd, t_lst **lst)
 {
@@ -25,25 +46,32 @@ static t_lst	*fd_new(int fd, t_lst **lst)
 	new->fd = fd;
 	new->next = NULL;
 	new->txt = NULL;
-
+	// si c'est la premiere fois qu'on lance GNL
+	if ((*lst) == NULL)
+		(*lst) = new;
+	else 
+		tmp->next = new;
+	return (new);
 }
 
 int				get_next_line(const int fd, char **line)
 {
 	static t_lst	*lst = NULL;
 	t_lst			*tmp;
-	char			buf[BUF_SIZE + 1];
 	
 	tmp = lst;
 	// possible de mettre ca dans le fd_new
-	if (fd == NULL || line == NULL)
+	if (fd < 0 || line == NULL)
 		return (-1);
 	// Gestion Muti fd -- Comprendre comment si j'appelle fd = 1 puis 2 puis 1 ca fonctionne
 	while (tmp && tmp->fd != fd)
 		tmp = tmp->next;
-	if (tmp == NULL)
+	if (tmp ==NULL)
 	{
 		if ((tmp = fd_new(fd, &lst)) == NULL)
 			return (-1);
 	}
+	if ((*line = write_line(tmp)) == NULL)
+		return (-1);
+	return (0);
 }
